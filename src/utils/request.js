ed5f51f304696e6030a7186a8840b8e60f2f321a
Justin Neff <neffjustin@gmail.com>
2017-02-10 08:18:52 -0600

    #2 - improved request

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,56 +1,113 @@
 import Promise from 'bluebird'
 import fetch from 'isomorphic-fetch'
 import moment from 'moment'
+import { Constants } from 'ims-shared-core'
+import { getCookie } from './cookies'
 import * as HeaderUtils from './headers'
 import RequestError from './RequestError'
 
+let storeInstance
+let tokensPathKey
+let packageId
+let hubUrl
+
+export const UPDATE_ACCESS_TOKEN = 'UPDATE_ACCESS_TOKEN'
+
+export function prepareRequest (store, tokensPath) {
+  if (store && store.getState && store.dispatch) {
+    storeInstance = store
+  } else {
+    throw new TypeError('Invalid "store" should be an instance of a redux store')
+  }
+
+  if (typeof tokensPath === 'string') {
+    tokensPathKey = tokensPath.split('.')
+  } else if (Array.isArray(tokensPath)) {
+    tokensPathKey = tokensPath
+  } else {
+    throw new TypeError('Invalid "tokensPath" should be either a string or an array')
+  }
+
+  packageId = getCookie(Constants.Cookies.PackageId)
+  if (!packageId) {
+    throw new Error('"PACKAGE_ID" cookie has not been set')
+  }
+
+  hubUrl = getCookie(Constants.Cookies.HubUrl)
+  if (!hubUrl) {
+    throw new Error('"HUB_URL" cookie has not been set')
+  }
+
+  scheduleRefreshAccessToken()
+}
+
 export default function request (url, options) {
   try {
+    verifyInitialized()
+
     const opts = prepareOptions(options)
 
     if (opts.authenticate) {
-      return getAccessToken(opts.packageId, opts.tokens, opts.hubUrl)
-        .then((accessToken) => {
-          opts.requestOptions.headers = HeaderUtils.createAuthenticatedRequestHeader(opts.packageId, accessToken)
-          return makeRequest(url, opts.requestOptions)
-        })
+      return makeAuthenticatedRequest(url, opts.requestOptions)
     } else {
-      opts.requestOptions.headers = HeaderUtils.createRequestHeader(opts.packageId)
-      return makeRequest(url, opts.requestOptions)
+      return makeUnauthenticatedRequest(url, opts.requestOptions)
     }
   } catch (e) {
     return Promise.reject(e)
   }
 }
 
+function getTokens () {
+  return storeInstance.getState().getIn(tokensPathKey)
+}
+
+function scheduleRefreshAccessToken () {
+  verifyInitialized()
+
+  const tokens = getTokens()
+
+  const refreshTime = moment.unix(tokens.expires).subtract(5, 'mintues')
+  const refreshIn = refreshTime.diff(moment())
+
+  setTimeout(refreshAccessToken, refreshIn)
+}
+
+function verifyInitialized () {
+  if (!storeInstance || !tokensPathKey || !packageId || !hubUrl) {
+    throw new Error('Request has not been prepared. You need to call "prepareRequest" to configure the request.')
+  }
+}
+
+function makeAuthenticatedRequest (url, requestOptions) {
+  const tokens = getTokens()
+  requestOptions.headers = HeaderUtils.createAuthenticatedRequestHeader(packageId, tokens.accessToken)
+  return makeRequest(url, requestOptions)
+}
+
+function makeUnauthenticatedRequest (url, requestOptions) {
+  requestOptions.headers = HeaderUtils.createRequestHeader(packageId)
+  return makeRequest(url, requestOptions)
+}
+
 function makeRequest (url, requestOptions) {
   return fetch(url, requestOptions).then(parseResponse)
 }
 
-function getAccessToken (packageId, tokens, hubUrl) {
-  const expiresLimit = moment().add(60, 'seconds').unix()
-  if (tokens.expires < expiresLimit) {
-    return refreshAccessToken(packageId, hubUrl, tokens.refreshToken)
-      .then((result) => {
-        if (result.success === true) {
-          return result.accessToken
-        } else {
-          throw new RequestError(result.message)
-        }
-      })
-  } else {
-    return Promise.resolve(tokens.accessToken)
-  }
-}
-
-function refreshAccessToken (packageId, hubUrl, refreshToken) {
+function refreshAccessToken () {
+  const tokens = getTokens()
   const refreshOptions = {
     headers: HeaderUtils.createRequestHeader(packageId),
     method: 'POST',
-    body: JSON.stringify({ refreshToken: refreshToken })
+    body: JSON.stringify({ refreshToken: tokens.refreshToken })
   }
 
-  return fetch(`${hubUrl}/users/accessToken`, refreshOptions).then(parseResponse)
+  return fetch(`${hubUrl}/users/accessToken`, refreshOptions)
+    .then(parseResponse)
+    .then((response) => {
+      storeInstance.dispatch(updateAccessToken(response.accessToken, response.expires))
+      scheduleRefreshAccessToken()
+      return response
+    })
 }
 
 function parseResponse (response) {
@@ -61,16 +118,16 @@ function parseResponse (response) {
   return response.json()
 }
 
+function updateAccessToken (accessToken, expires) {
+  return {
+    type: UPDATE_ACCESS_TOKEN,
+    accessToken: accessToken,
+    expires: expires
+  }
+}
+
 function prepareOptions (options = {}) {
   const opts = Object.assign({}, options)
-
-  if (!opts.packageId) {
-    throw new RequestError('Missing packageId in options')
-  }
-
-  if (!opts.hubUrl) {
-    throw new RequestError('Missing hubUrl in options')
-  }
 
   if (!opts.method) {
     opts.method = 'GET'
@@ -82,10 +139,6 @@ function prepareOptions (options = {}) {
 
   if (opts.authenticate === undefined) {
     opts.authenticate = true
-  }
-
-  if (opts.authenticate && (!opts.tokens || !opts.tokens.refreshToken || !opts.tokens.accessToken)) {
-    throw new RequestError(`Invalid or missing tokens object in options: ${JSON.stringify(opts.tokens)}`)
   }
 
   const requestOptions = {
