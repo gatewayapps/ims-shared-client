83e05172308251f87e6e92863eb53a08692ff775
Daniel Gary <daniel@gatewayapps.com>
2017-07-19 21:57:23 -0500

    added in ReactiveLayout

--- src/app/index.js
+++ src/app/index.js
@@ -2,6 +2,10 @@ import ImsBootstrapper from './bootstrapper'
 import { injectReducer } from './store/reducers'
 import { injectSaga } from './store/sagas'
 import { selectLocationState } from './modules/routing'
+
+/* Layouts */
+import ReactiveLayout from './layouts/ReactiveLayout'
+
 import {
   logout,
   SECURITY_LOGOUT,
@@ -19,5 +23,6 @@ module.exports = {
   selectAccessToken: selectAccessToken,
   selectCurrentUser: selectCurrentUser,
   selectLocationState: selectLocationState,
-  selectTokens: selectTokens
+  selectTokens: selectTokens,
+  ReactiveLayout: ReactiveLayout
 }
