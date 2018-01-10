174664104ec68fa893d748470fb0fd2fe8f58170
Daniel Gary <daniel@gatewayapps.com>
2018-01-10 07:58:00 -0600

    added className to SecureLink

--- src/components/SecureLink/SecureLink.js
+++ src/components/SecureLink/SecureLink.js
@@ -12,10 +12,10 @@ export default class SecureLink extends BaseComponent {
   render () {
     if (this.checkPermission(`+:${this.props.packageId || PackageInformation.packageId}:${this.props.role || 'user'}:${this.props.permission}:*:*`, this.props.user, true)) {
       return (
-        <Link to={this.props.to} activeClassName='active'>{this.props.children}</Link>
+        <Link to={this.props.to} activeClassName='active' className={this.props.className}>{this.props.children}</Link>
       )
     } else {
-      return <span />
+      return null
     }
   }
 }
@@ -26,5 +26,6 @@ SecureLink.propTypes = {
   user: React.PropTypes.object.isRequired,
   role: React.PropTypes.string,
   permission: React.PropTypes.string.isRequired,
+  className: React.PropTypes.any,
   to: React.PropTypes.string.isRequired
 }
