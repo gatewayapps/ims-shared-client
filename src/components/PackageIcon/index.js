36fb6429bc3278ffeaa834d8aded9370b9b7be5b
Daniel Gary <daniel@gatewayapps.com>
2018-05-14 08:34:30 -0500

    added support for icon images

---
+++ src/components/PackageIcon/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default class PackageIcon extends React.PureComponent {
+  render () {
+    switch (this.props.package.source) {
+      case 'fa': {
+        return (
+
+          <i className={`fa fa-fw ${this.props.package.icon}`} />
+        )
+      }
+      case 'img': {
+        const imageSources = [
+          `${this.props.package.url}/icons/tile-64.png`,
+          `${this.props.package.url}/icons/tile-128.png 1.5x`,
+          `${this.props.package.url}/icons/tile-256.png 2x`,
+          `${this.props.package.url}/icons/tile-512.png 3x`
+        ]
+        return <img className='package-tile-image' src={imageSources[0]} srcSet={imageSources.join(', ')} alt={this.props.package.name} />
+      }
+      default: {
+        return null
+      }
+    }
+  }
+}
+
+PackageIcon.propTypes = {
+  package: PropTypes.object.isRequired
+}
--- src/components/PackageTile/PackageTile.js
+++ src/components/PackageTile/PackageTile.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import PackageIcon from '../PackageIcon'
 import '../../styles/PackageTile.css'
 export default class PackageTile extends React.Component {
   render () {
-    const packageIcon = `fa fa-fw ${this.props.package.icon}`
     const backgroundColor = this.props.package.color ? '#' + this.props.package.color.replace('#', '') : '#373737'
     return (
       <div className='package-tile' style={{ backgroundColor: backgroundColor, cursor: 'pointer' }}>
         <a className='package-tile-link' href={this.props.package.url} target='__blank'>
           <div className='package-tile-opacity' >
-            <div className='package-tile-icon'><i className={packageIcon} /></div>
+            <div className='package-tile-icon'>
+              <PackageIcon package={this.props.package} />
+            </div>
             <div className='package-tile-title'>{this.props.package.name}</div>
 
             {
