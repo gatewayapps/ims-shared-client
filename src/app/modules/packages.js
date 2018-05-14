6ed824e02bfb5aee8f9a24d6d9ca43dce5100e8f
Daniel Gary <daniel@gatewayapps.com>
2018-05-14 08:37:01 -0500

    fixed an issue with zeroing out badge count when refreshing

--- src/app/modules/packages.js
+++ src/app/modules/packages.js
@@ -34,6 +34,14 @@ export function setPackageBadgeCount (id, count) {
 // ------------------------------------
 const ACTION_HANDLERS = {
   [SET_PACKAGES_STATE]: (state, action) => {
+   // Prevent zeroing out the badge count during an update
+    for (var i = 0; i < state.length; i++) {
+      const packageIndex = action.packages.findIndex((p) => p.packageId === state[i].packageId)
+      if (packageIndex > -1) {
+        action.packages[packageIndex].badgeCount = state[i].badgeCount
+      }
+    }
+
     return fromJS(action.packages)
   },
   [SET_PACKAGE_BADGE_COUNT]: (state, action) => {
