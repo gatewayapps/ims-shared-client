1b1190c4714f113072bc59407553428bda6db088
John Murphy <jmichealmurphy@gmail.com>
2018-05-17 15:16:08 -0500

    badge support

--- src/components/Header/Bootstrap3Header.js
+++ src/components/Header/Bootstrap3Header.js
@@ -13,10 +13,12 @@ class Bootstrap3Header extends React.PureComponent {
 
   renderMenuItemChildren (parentText, children) {
     const linkChildren = children.map((link) => {
+      const badge = link.badgeCount > 0 ? <span className={`badge ${link.badgeClass}`}>{link.badgeCount}</span> : null
+
       return (
         <li key={link.text} className='dropdown-item nav-link'>
           <Link key={link.text} to={link.url} label={link.text}>
-            <i className={link.faIcon} /> {link.text}
+            <i className={link.faIcon} /> {link.text} {badge}
           </Link>
         </li>
       )
@@ -34,20 +36,23 @@ class Bootstrap3Header extends React.PureComponent {
     if (this.props.menuItems) {
       const linkElements = this.props.menuItems.map((link) => {
         if (link.children && link.children.length > 0) {
+          const badge = link.badgeCount > 0 ? <span className={`badge ${link.badgeClass}`}>{link.badgeCount}</span> : null
           return (
             <li className='nav-item dropdown'>
               <a className='nav-link dropdown-toggle' href='#' id={`${link.text}Dropdown`}
                 data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
-                <i className={link.faIcon} /> {link.text} <span className='caret' />
+                <i className={link.faIcon} /> {link.text} {badge} <span className='caret' />
               </a>
               {this.renderMenuItemChildren(link.text, link.children)}
             </li>
           )
         } else {
+          const badge = link.badgeCount > 0 ? <span className={`badge ${link.badgeClass}`}>{link.badgeCount}</span> : null
+
           return (
             <li key={link.text} className='nav-item'>
               <Link key={link.text} to={link.url} className='nav-link' label={link.text}>
-                <i className={link.faIcon} aria-hidden='true' /> {link.text}
+                <i className={link.faIcon} aria-hidden='true' /> {link.text} {badge}
               </Link>
             </li>
           )
